b6280ff577287485b1e50eee9bd6fb78f096c89a
marcfolks <marcfolks@gmail.com>
2017-06-20 17:37:59 -0400

    just 3 and 5

--- print_to_screen.rb
+++ print_to_screen.rb
@@ -1,6 +1,6 @@
 puts
 (1..100).each do|n|
-  mined = if n % 3 == 0 then 'mined' end
-  	minds =if n % 5 == 0 then 'minds' end
+  mined = if n == 3 then 'mined' end
+  	minds =if n == 5 then 'minds' end
   puts "#{n} #{mined}#{minds}"
 end
