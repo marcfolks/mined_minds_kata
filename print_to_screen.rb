57bb1faea2226a20d10469d965f1490fc01fb5d6
marcfolks <marcfolks@gmail.com>
2017-06-22 12:22:38 -0400

    mined= every 3rd

--- print_to_screen.rb
+++ print_to_screen.rb
@@ -1,9 +1,5 @@
 1.upto(100) do |i|
-  if i % 5 == 0 and i % 3 == 0
-    puts "minedminds"
-  elsif i % 5 == 0
-    puts "minds"
-  elsif i % 3 == 0
+  if i % 3 == 0
     puts "mined"
   else
 	puts i
