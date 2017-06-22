a894462a8a24684a5d9e5307f9d3ef1057e6d633
marcfolks <marcfolks@gmail.com>
2017-06-22 12:49:54 -0400

    mindedminds3&5

--- print_to_screen.rb
+++ print_to_screen.rb
@@ -1,7 +1,9 @@
 1.upto(100) do |i|
-  if i % 3 == 0 and i % 5 == 0
-    puts "mindedminds"
-  else
+  if i % 3 == 0 
+  	puts "minded"
+  elsif i % 5 == 0
+  	puts "minds"
+    else
 	puts i
   end
 end
