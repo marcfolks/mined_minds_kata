75c48f0f0d0b2a4aad5d4931271f32a43c0056f8
marcfolks <marcfolks@gmail.com>
2017-06-22 11:08:58 -0400

    other 1:100 solutin

--- print_to_screen.rb
+++ print_to_screen.rb
@@ -1,6 +1,12 @@
-puts
-(1..100).each do|n|
-  mined = if n == 3 then 'mined' end
-  	minds =if n == 5 then 'minds' end
-  puts "#{n} #{mined}#{minds}"
+1.upto(100) do |i|
+  if i % 5 == 0 and i % 3 == 0
+    puts "minedminds"
+  elsif i % 5 == 0
+    puts "minds"
+  elsif i % 3 == 0
+    puts "mined"
+  else
+	puts i
+  end
 end
+  
